b5e32b90cad2f1e806ff18cb280221e3d27ee3ea
Elisei123 <43387542+Elisei123@users.noreply.github.com>
2020-02-26 21:17:24 +0200

    Create database with social-profiles

--- Web/admin.py
+++ Web/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from Web.models import retele_de_socializare_utilizator
 # Register your models here.
+
+class retele_de_socializare_utilizatorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'github', 'facebook', 'instagram', 'gmail', 'youtube', 'linkedin', 'discord', 'skype', 'steam', 'paypal')
+
+admin.site.register(retele_de_socializare_utilizator, retele_de_socializare_utilizatorAdmin)
--- Web/models.py
+++ Web/models.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+class retele_de_socializare_utilizator(models.Model):
+    github = models.CharField(max_length=40, null=True)
+    facebook = models.CharField(max_length=40, null=True)
+    instagram = models.CharField(max_length=40, null=True)
+    gmail = models.CharField(max_length=40, null=True)
+    youtube = models.CharField(max_length=40, null=True)
+    linkedin = models.CharField(max_length=40, null=True)
+    discord = models.CharField(max_length=40, null=True)
+    skype = models.CharField(max_length=40, null=True)
+    steam = models.CharField(max_length=40, null=True)
+    paypal = models.CharField(max_length=40, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
