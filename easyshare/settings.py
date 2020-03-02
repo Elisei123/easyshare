74fd004d45f319073dbf6789cc0528086d297ab0
Elisei123 <43387542+Elisei123@users.noreply.github.com>
2020-03-02 18:51:32 +0200

    update v2 mailo

--- easyshare/settings.py
+++ easyshare/settings.py
@@ -83,6 +83,13 @@ DATABASES = {
     }
 }
 
+import os
+import dj_database_url
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ['DATABASE_URL'], conn_max_age=600)
+    }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
