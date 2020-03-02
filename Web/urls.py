31bfa322163a0e6558f45e60c6baa9272577d719
Elisei123 <43387542+Elisei123@users.noreply.github.com>
2020-03-02 10:20:42 +0200

    database-connection

--- Web/urls.py
+++ Web/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('profile/<name_profile>', views.name_profile_function, name='name_profile'),
 ]
--- Web/views.py
+++ Web/views.py
@@ -2,12 +2,51 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from .models import retele_de_socializare_utilizator
 
+
 # Create your views here.
 
 def home(request):
-    if request.method == "GET": # Pt. render pagina cand este ceruta cu "GET"
+
+    # Pt. render pagina cand este ceruta cu "GET" and user is login
+    if request.method == "GET" and request.user.is_authenticated:
+        # Create a object when doesn't exist.
+        if not retele_de_socializare_utilizator.objects.filter(user=request.user).exists():
+            # up to object 'retele_de_socializare_utilizator'
+            retele_de_socializare_utilizator_object = retele_de_socializare_utilizator(
+                user=request.user,
+                github="https://github.com/",
+                facebook="https://www.facebook.com/",
+                instagram="https://www.instagram.com/",
+                gmail="",
+                youtube="https://www.youtube.com/channel/",
+                linkedin="https://www.linkedin.com/in/",
+                discord="",
+                skype="",
+                steam="https://steamcommunity.com/profiles/",
+                paypal="paypal.me/",
+            )
+            retele_de_socializare_utilizator_object.save()
+
+            print("Obiectul a fost creat (user:  ", request.user, ")")
+            return redirect('home')
+
         # Put inputs from database
-        return render(request, 'home.html')
+        inputs_retele_socializare = retele_de_socializare_utilizator.objects.filter(user=request.user).first()
+        return render(
+            request,
+            'home.html',
+            {
+                'inputs_retele_socializare': inputs_retele_socializare,
+            }
+        )
+    # Pt. render pagina cand este ceruta cu "GET" and user is NOT login
+    if request.method == "GET":
+        return render(
+            request,
+            'home.html',
+        )
+
+    ## When you click on "UPDATE"
 
     # Take inputs
     github = request.POST['Github_input']
@@ -20,28 +59,6 @@ def home(request):
     skype = request.POST['Skype_input']
     steam = request.POST['Steam_input']
     paypal = request.POST['Paypal_input']
-
-    ## When you click on "UPDATE"
-    if not retele_de_socializare_utilizator.objects.filter(user=request.user).exists():
-
-        # up to object 'retele_de_socializare_utilizator'
-        retele_de_socializare_utilizator_object = retele_de_socializare_utilizator(
-            user=request.user,
-            github=github,
-            facebook=facebook,
-            instagram=instagram,
-            gmail=gmail,
-            youtube=youtube,
-            linkedin=linkedin,
-            discord=discord,
-            skype=skype,
-            steam=steam,
-            paypal=paypal
-        )
-        retele_de_socializare_utilizator_object.save()
-
-        print("Obiectul a fost creat (user:  ", request.user, ")")
-        return redirect('home')
 
     # if object exist in database, you must update on them
     retele_de_socializare_utilizator.objects.filter(user=request.user).update(
@@ -57,8 +74,14 @@ def home(request):
         steam=steam,
         paypal=paypal
     )
+
     print("Obiectul a fost modificat (user:  ", request.user, ")")
     return redirect('home')
 
-#TODO: Vezi ca se salveaza o singura coloana(atribut) P.S: Este pt ca ia din inputs, si in inputs sunt mereu default.
-#TODO: Cred ca trebuie sa ii dai update in value, si astfel se ia bine ;)
+def name_profile_function(request, name_profile):
+    return render(
+        request, 'profile_show.html',
+        {
+            'name_profile': name_profile
+        }
+    )
